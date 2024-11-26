4dfed5bd24d6d2302446ad0f01095903ab19ddb4
Anna Safonova <annasafonova.info@gmail.com>
2024-11-26 15:59:48 +0300

    add routes

--- src/Shelly.tsx
+++ src/Shelly.tsx
@@ -1,9 +1,23 @@
-import { Facebook } from './ui';
+import { Routes, Route } from 'react-router';
 
 function Shelly() {
 	return (
 		<div className='mx-auto max-w-screen-xl'>
-			<Facebook />
+			{/* <Header/> */}
+			<main>
+				<Routes>
+					<Route path='/' element={<div>Main page</div>} />
+					<Route path='/shop' element={<div>Shop page</div>} />
+					<Route path='/shop/:id' element={<div>Poduct page</div>} />
+					<Route path='/cart' element={<div>Cart page</div>} />
+					<Route path='/admindb' element={<div>Admin page</div>} />
+					<Route path='/account' element={<div>Account page</div>} />
+					<Route path='/register' element={<div>Register page</div>} />
+					<Route path='/login' element={<div>Login page</div>} />
+					<Route path='*' element={<div>404 page</div>} />
+				</Routes>
+			</main>
+			{/* <Footer/> */}
 		</div>
 	);
 }
